5401ea2fb9c59885b18f2eaa5f2d0ef094f2dbaa
nv22142-ali <160000145+nv22142-ali@users.noreply.github.com>
2024-02-25 09:00:47 +0000

    3 - Creating Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import { images } from "../constants"
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
+  return(<sectio>
+    <Header className="container mx-auto px-5 flex justify-between">
+      <div>
+          <img src={images.Logo} alt ="logo" />
+      </div>
+      <div className="flex gap-x-9">
+        <ul className="flex gap-x-5">
+          <li>
+            <a herf="/">Home</a>
+          </li>
+          <li>
+            <a herf="/">Articales</a>
+          </li>
+          <li>
+            <a herf="/">Pages</a>
+          </li>
+          <li>
+            <a herf="/">Pricing</a>
+          </li>
+          <li>
+            <a herf="/">Faq</a>
+          </li>
+        </ul>
+        <butten>Sign in</butten>
+      </div>
+    </Header>
+  </sectio>) 
 }
 
-export default Header
+export default Header;
