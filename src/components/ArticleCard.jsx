27918b2ab99e73521c9b2e51bc9249d500c1686d
nv22142-ali <160000145+nv22142-ali@users.noreply.github.com>
2024-03-11 06:50:36 +0000

    6 - Article card component

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React from "react";
 import { BsCheckLg } from "react-icons/bs";
+
 import { images } from "../constants";
 
-const ArticleCard = (className) => {
+const ArticleCard = ({ className }) => {
   return (
     <div
       className={`rounded-xl overflow-hidden shadow-[rgba(7,_65,_210,_0.1)_0px_9px_30px] ${className}`}
@@ -20,27 +21,33 @@ const ArticleCard = (className) => {
           Majority of peole will work in jobs that don’t exist today.
         </p>
         <div className="flex justify-between flex-nowrap items-center mt-6">
-        <div className="flex items-center gap-x-2 md:gap-x-2.5">
-            <img src={images.PostProfileImage} alt="post profile" />
+          <div className="flex items-center gap-x-2 md:gap-x-2.5">
+            <img
+              src={images.PostProfileImage}
+              alt="post profile"
+              className="w-9 h-9 md:w-10 md:h-10"
+            />
             <div className="flex flex-col">
-            <h4 className="font-bold italic text-dark-soft text-sm md:text-base">
+              <h4 className="font-bold italic text-dark-soft text-sm md:text-base">
                 Viola Manisa
               </h4>
               <div className="flex items-center gap-x-2">
-              <span className="bg-[#36B37E] w-fit bg-opacity-20 p-1.5 rounded-full">
-                <BsCheckLg className="w-1.5 h-1.5 text-[#36B37E]" />
+                <span className="bg-[#36B37E] w-fit bg-opacity-20 p-1.5 rounded-full">
+                  <BsCheckLg className="w-1.5 h-1.5 text-[#36B37E]" />
                 </span>
-              <span className="italic text-dark-light text-xs md:text-sm">
-              Verified writer
-              </span>
+                <span className="italic text-dark-light text-xs md:text-sm">
+                  Verified writer
+                </span>
               </div>
             </div>
+          </div>
+          <span className="font-bold text-dark-light italic text-sm md:text-base">
+            02 May
+          </span>
         </div>
-        <span className="font-bold text-dark-light italic text-sm md:text-base">02 May</span>
-        </div>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ArticleCard
+export default ArticleCard;
