109c28ba212d76ca3651b28dc31d7f99429eb726
nv22142-ali <160000145+nv22142-ali@users.noreply.github.com>
2024-03-13 14:25:20 +0000

    10 - comments section with replying

--- src/pages/articleDetail/ArticleDetailPage.jsx
+++ src/pages/articleDetail/ArticleDetailPage.jsx
@@ -4,6 +4,8 @@ import MainLayout from '../../components/MainLayout'
 import BreadCrumbs from '../../components/BreadCrumbs';
 import { images } from '../../constants';
 import SuggestedPosts from './container/SuggestedPosts';
+import CommentsContainer from '../../components/comments/CommentsContainer';
+
 
 const breadCrumbsData = [
   {name: "Home", link: '/'},
@@ -65,6 +67,7 @@ const ArticleDetailPage = () => {
         <div className='mt-4 text-dark-soft '>
         <p className='leading-7'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas purus viverra accumsan in nisl nisi. Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque. In egestas erat imperdiet sed euismod nisi porta lorem mollis. Morbi tristique senectus et netus. Mattis pellentesque id nibh tortor id aliquet lectus proin.</p>
         </div>
+        <CommentsContainer className="mt-10" logginedUserId="a" />
       </article>
       <SuggestedPosts  header="Latest Article" posts={postsData} tags={tagsData} className="mt-8 lg:mt-0 lg:max-w-xs"/>
     </section>
